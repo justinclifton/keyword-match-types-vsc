d8b9479ba0b3368d5daa4eca19f901fbf8cc836c
Justin Clifton <justinclifton@Justins-iMac-Pro.local>
2020-08-29 22:09:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,32 @@
+# Keyword MatchTypes README
+
+This extension is useful for anyone that uses VS code to work with keyword match types. You can use the commands in this extension to quickly and easily convert between various match types.
+
+## Features
+
+This extension adds 4 commands to VS code for the 4 different keyword matchtypes. Match type examples and descriptions of the commands are below. Simply highlight the lines of text that you want to convert to a specific match type and then run the corresponding command.
+
+[exact match]
+"phrase match"
++broad +match +modified
+broad match
+
+Exact Match: Returns lines of text which are wrapped in brackets. If this command is run on text lines that are arleady exact match, it returns text lines with no special keyword formatting (in other words, broad match). By default this command is associated with the keyboard shortcut "shift + alt + e" but it can also be run by directly searching for the command after pressing "shift + cmd + p" (for mac) or "shift + ctrl + p" (for Windows).
+
+Phrase Match: Returns lines of text which are wrapped in double quotes. If this command is run on text lines that are arleady phrase match, it returns text lines with no special keyword formatting (in other words, broad match). By default this command is associated with the keyboard shortcut "shift + alt + p" but it can also be run by directly searching for the command after pressing "shift + cmd + p" (for mac) or "shift + ctrl + p" (for Windows).
+
+Broad Match Modified (BMM): Returns lines of text with each word starting with a plus sign. If this command is run on text lines that are arleady BMM, it returns text lines with no special keyword formatting (in other words, broad match). By default this command is associated with the keyboard shortcut "shift + alt + m" but it can also be run by directly searching for the command after pressing "shift + cmd + p" (for mac) or "shift + ctrl + p" (for Windows).
+
+Broad Match: Returns lines of text with no special match type formatting. If this command is run on text lines that are arleady broad match, it doesn't change anything. By default this command is associated with the keyboard shortcut "shift + alt + b" but it can also be run by directly searching for the command after pressing "shift + cmd + p" (for mac) or "shift + ctrl + p" (for Windows).
+
+## Known Issues
+
+There are no known issues with this extension at this time.
+
+## Release Notes
+
+### 1.0.0
+
+Initial release of the extension.
+
+-----------------------------------------------------------------------------------------------------------
---
+++ extension.js
@@ -0,0 +1,245 @@
+// The module 'vscode' contains the VS Code extensibility API
+// Import the module and reference it with the alias vscode in your code below
+const vscode = require("vscode");
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+
+/**
+ * @param {vscode.ExtensionContext} context
+ */
+
+function activate(context) {
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with  registerCommand
+	// The commandId parameter must match the command field in package.json
+
+	let disposable = vscode.commands.registerCommand("extension.exactMatch", function() {
+		let editor = vscode.window.activeTextEditor;
+		let lines = [];
+		let updatedLines = [];
+		let cleanLine;
+		let newLine;
+		let selectionLength;
+
+		const selection = editor.selection;
+		const startPoint = selection.start.line;
+		const endPoint = selection.end.line;
+		const isSingleLine = selection.isSingleLine;
+
+		if (isSingleLine) {
+			selectionLength = editor.document.getText(selection).length;
+			lines = editor.document.getText(selection).replace(/\s/g, "").split(",");
+		} else {
+			selectionLength = editor.document.lineAt(endPoint).text.length;
+			for (let i = startPoint; i <= endPoint; i++) {
+				lines.push(editor.document.lineAt(i).text);
+			}
+		}
+
+		for (let i = 0; i < lines.length; i++) {
+			if (lines[i]) {
+				if (lines[i].includes("+") || lines[i].includes('"')) {
+					//removes non exact characters from the line if they exist
+					cleanLine = lines[i].replace(/\+|"/g, "");
+				} else {
+					cleanLine = lines[i];
+				}
+
+				if (lines[i].includes("[") || lines[i].includes("]")) {
+					//removes exact characters if they exist
+					newLine = cleanLine.replace(/\[|\]/g, "");
+					updatedLines.push(newLine);
+				} else {
+					newLine = cleanLine.replace(/^/g, "[").replace(/$/g, "]"); //adds exact characters if they don't exist
+					updatedLines.push(newLine);
+				}
+			}
+		}
+
+		editor.edit((editBuilder) => {
+			isSingleLine
+				? editBuilder.replace(selection, updatedLines.join(", "))
+				: editBuilder.replace(
+						new vscode.Range(startPoint, 0, endPoint, selectionLength),
+						updatedLines.join("\n")
+					);
+		});
+	});
+
+	disposable = vscode.commands.registerCommand("extension.phraseMatch", function() {
+		let editor = vscode.window.activeTextEditor;
+		let lines = [];
+		let updatedLines = [];
+		let cleanLine;
+		let newLine;
+		let selectionLength;
+
+		const selection = editor.selection;
+		const startPoint = selection.start.line;
+		const endPoint = selection.end.line;
+		const isSingleLine = selection.isSingleLine;
+
+		if (isSingleLine) {
+			selectionLength = editor.document.getText(selection).length;
+			lines = editor.document.getText(selection).replace(/\s/g, "").split(",");
+		} else {
+			selectionLength = editor.document.lineAt(endPoint).text.length;
+			for (let i = startPoint; i <= endPoint; i++) {
+				lines.push(editor.document.lineAt(i).text);
+			}
+		}
+
+		for (let i = 0; i < lines.length; i++) {
+			if (lines[i]) {
+				//if line exists
+
+				lines[i].trim(); //removes leading or trailing whitespace from the line
+
+				if (lines[i].includes("+") || lines[i].includes("[") || lines[i].includes("]")) {
+					//removes non phrase characters from the line if they exist
+					cleanLine = lines[i].replace(/\+|\[|\]/g, "");
+				} else {
+					cleanLine = lines[i];
+				}
+
+				if (lines[i].includes('"')) {
+					//remove phrase syntax if it already exists
+					newLine = cleanLine.replace(/"/g, "");
+					updatedLines.push(newLine);
+				} else {
+					newLine = cleanLine.replace(/^/g, '"').replace(/$/g, '"'); //add phrase syntax if it doesn't exist
+					updatedLines.push(newLine);
+				}
+			}
+		}
+
+		editor.edit((editBuilder) => {
+			isSingleLine
+				? editBuilder.replace(selection, updatedLines.join(", "))
+				: editBuilder.replace(
+						new vscode.Range(startPoint, 0, endPoint, selectionLength),
+						updatedLines.join("\n")
+					);
+		});
+	});
+
+	disposable = vscode.commands.registerCommand("extension.BMM", function() {
+		let editor = vscode.window.activeTextEditor;
+		let lines = [];
+		let updatedLines = [];
+		let cleanLine;
+		let newLine;
+		let selectionLength;
+
+		const selection = editor.selection;
+		const startPoint = selection.start.line;
+		const endPoint = selection.end.line;
+		const isSingleLine = selection.isSingleLine;
+
+		if (isSingleLine) {
+			selectionLength = editor.document.getText(selection).length;
+			lines = editor.document.getText(selection).replace(/\s/g, "").split(",");
+		} else {
+			selectionLength = editor.document.lineAt(endPoint).text.length;
+			for (let i = startPoint; i <= endPoint; i++) {
+				lines.push(editor.document.lineAt(i).text);
+			}
+		}
+
+		for (let i = 0; i < lines.length; i++) {
+			if (lines[i]) {
+				lines[i].trim(); //removes leading or trailing whitespace from the line
+
+				//removes non BMM characters from the line if they exist
+				if (lines[i].includes("[") || lines[i].includes("]") || lines[i].includes('"')) {
+					cleanLine = lines[i].replace(/\[|\]|"/g, "");
+				} else {
+					cleanLine = lines[i];
+				}
+
+				if (lines[i].includes("+")) {
+					//removes BMM characters if they exist
+					newLine = cleanLine.replace(/\+/g, "");
+					updatedLines.push(newLine);
+				} else {
+					newLine = cleanLine.replace(/^/g, "+").replace(/\s/g, " +"); //adds BMM characters if they don't exist
+					updatedLines.push(newLine);
+				}
+			}
+		}
+
+		editor.edit((editBuilder) => {
+			isSingleLine
+				? editBuilder.replace(selection, updatedLines.join(", "))
+				: editBuilder.replace(
+						new vscode.Range(startPoint, 0, endPoint, selectionLength),
+						updatedLines.join("\n")
+					);
+		});
+	});
+
+	disposable = vscode.commands.registerCommand("extension.broadMatch", function() {
+		let editor = vscode.window.activeTextEditor;
+		let lines = [];
+		let updatedLines = [];
+		let newLine;
+		let selectionLength;
+
+		const selection = editor.selection;
+		const startPoint = selection.start.line;
+		const endPoint = selection.end.line;
+		const isSingleLine = selection.isSingleLine;
+
+		if (isSingleLine) {
+			selectionLength = editor.document.getText(selection).length;
+			lines = editor.document.getText(selection).replace(/\s/g, "").split(",");
+		} else {
+			selectionLength = editor.document.lineAt(endPoint).text.length;
+			for (let i = startPoint; i <= endPoint; i++) {
+				lines.push(editor.document.lineAt(i).text);
+			}
+		}
+
+		for (let i = 0; i < lines.length; i++) {
+			if (lines[i]) {
+				lines[i].trim(); //removes leading or trailing whitespace from the line
+
+				//removes all characters for BMM, exact, & phrase, reverts line to regular broad match
+				if (
+					lines[i].includes("+") ||
+					lines[i].includes('"') ||
+					lines[i].includes("[") ||
+					lines[i].includes("]")
+				) {
+					newLine = lines[i].replace(/\+|\[|\]|"/g, "");
+					updatedLines.push(newLine);
+				} else {
+					//if line is already broad match leave it unchanged
+					newLine = lines[i];
+					updatedLines.push(newLine);
+				}
+			}
+		}
+
+		editor.edit((editBuilder) => {
+			isSingleLine
+				? editBuilder.replace(selection, updatedLines.join(", "))
+				: editBuilder.replace(
+						new vscode.Range(startPoint, 0, endPoint, selectionLength),
+						updatedLines.join("\n")
+					);
+		});
+	});
+
+	context.subscriptions.push(disposable);
+}
+exports.activate = activate;
+
+// this method is called when your extension is deactivated
+function deactivate() {}
+
+module.exports = {
+	activate,
+	deactivate
+};
